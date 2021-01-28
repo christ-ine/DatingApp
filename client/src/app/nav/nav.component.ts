c89dd4146046224147e0344b4d6ed0bc1eaa02c8
Christine Ross <christinejoyross@gmail.com>
2021-01-27 17:53:40 -0800

    homepage register form

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -7,14 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {}
+  
 
-  constructor() { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+    
   }
 
   login(){
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
   }
+
+  logout() {
+    this.accountService.logout();
+  }
+
+ 
 
 }
