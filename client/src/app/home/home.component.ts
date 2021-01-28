56b120e50164c543f93ae86ba882c96641238860
Christine Ross <christinejoyross@gmail.com>
2021-01-27 19:15:47 -0800

    login and register functionality

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +16,11 @@ export class HomeComponent implements OnInit {
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
   }
 
 }
